7b52308e5ddbf33cef1896cfa600f20231986d67
NithishKolli <nithish.1993@gmail.com>
2014-01-19 18:03:59 +0530

    second Test

---
+++ pages/admission.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admission</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="admission" />
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+
+    <link rel="stylesheet" type="text/css" href="css/profiles.css">
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/func.js"></script>
+
+</head>
+<body>
+    <?php include_once('navbar.php');
+    ?>
+    <script type="text/javascript">
+        changeActive(3);
+    </script>
+    <div class = "container" style="margin-top:70px;">
+        <h1 style="text-align:center;">Admissions</h1>
+        <div class="row">
+        <div class="col-md-3">
+            <ul class="nav nav-pills nav-stacked" style="position: fixed; width: 10%;">
+                <li class="active"><a href="#about">About Us</a></li>
+                <li><a href="#facilities">Facilities</a></li>
+                <li><a href="#galley">Gallery</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+</body>
---
+++ pages/feedback.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admission</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="admission" />
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/component.css" />
+
+    <link rel="stylesheet" type="text/css" href="css/profiles.css">
+    <script src="js/modernizr.custom.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/func.js"></script>
+
+</head>
+<body>
+<?php include_once('navbar.php');
+?>
+<script type="text/javascript">
+    changeActive(1);
+</script>
+</body>
