3d04ec3c8a8e20f05bf978092628923973ba1ce5
ankeshanand <ankeshanand1994@gmail.com>
2014-01-20 01:49:21 +0530

    Add slider, Homepage modifications

--- pages/navbar.php
+++ pages/navbar.php
@@ -7,15 +7,15 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="resources/crest.jpg" /></a></a>
+            <a class="navbar-brand" href="#"><img src="../resources/crest.jpg" /></a></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse" id="navbar">
             <ul class="nav navbar-nav" id="navbarlist">
-                <li class="active"><a href="index.php">Home</a></li>
+                <li class="active"><a href="../index.php">Home</a></li>
                
-                <li><a href="pages/department.php">Department</a></li>
+                <li><a href="department.php">Department</a></li>
                 <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Academics<b class="caret"></b></a>
                 <ul class="dropdown-menu">
